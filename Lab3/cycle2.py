33062e3765e083ea098058247232c7b7e19ea83e
DKuzn <58743394+DKuzn@users.noreply.github.com>
2020-02-16 21:27:44 +0300

    Update cycle2.py

--- Lab3/cycle2.py
+++ Lab3/cycle2.py
@@ -2,7 +2,7 @@ for i in range(10):
     x = float(input("Введите x: "))
     y = float(input("Введите y: "))
 
-    if y * y + x * x < 9 and x >= 0:
+    if y ** 2 + x ** 2 < 9 and x >= 0:
         print("Выстрел", i+1, "с координатами", "(", x, ",", y, ")", "попал в мишень.")
     elif y < abs(x) and y == 3 and y < -x and y == -3 and x < 0:
         print("Выстрел", i+1, "с координатами", "(", x, ",", y, ")", "попал в мишень.")
