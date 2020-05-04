e012ad6fbd595e22e64451a114fc53e7829041a1
Dmitry Kuznetsov <58743394+DKuzn@users.noreply.github.com>
2020-05-04 16:27:21 +0300

    Add files via upload

---
+++ func_plot.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def func(x):
+    return np.sqrt((10 ** 10) / ((10 ** 4 - 4 * np.pi ** 2 * x ** 2) + 4 * np.pi ** 2 * x ** 2))
+
+
+x = np.linspace(10**8, 10**12, 10**9, dtype=np.float64)
+y = np.float64(func(x))
+print(len(x))
+
+plt.loglog(x, y, 'g')
+#plt.ylabel('Время')
+#plt.xlabel('Длина массива')
+plt.title("Модуль передаточной функции")
+plt.tight_layout()
+plt.grid()
+plt.show()
+plt.savefig()
