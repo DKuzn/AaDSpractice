6aa28bb4d94ba1e1e35d5e82040b2d6bb035c14f
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-05-09 20:02:46 +0300

    update directories

--- Hash table/MainWindow.py
+++ Hash table/MainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Lab6/MainWindow.ui'
+# Form implementation generated from reading ui file 'Hash table/MainWindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.14.2
 #
