363e430a670535cd7f860ca12f81992265d198df
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-05-13 13:59:36 +0300

    fix import

---
+++ Linked_list/main.py
@@ -0,0 +1,15 @@
+import Linked_list.linked_list as list
+import random as rd
+
+polynomial = list.MyLinkedList()
+
+
+def polynom(x, n):
+    for i in range(n, -1, -1):
+        p = rd.randint(0, 10) * x ** i
+        if p != 0:
+            polynomial.append(p)
+    return polynomial
+
+
+print(polynom(2, 6))
