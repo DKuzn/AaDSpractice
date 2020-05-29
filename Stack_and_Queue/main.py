5e2f952d1ed86871639ae1cf0b355d1b9cc33839
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-05-29 19:07:18 +0300

    some fix

--- Stack_and_Queue/main.py
+++ Stack_and_Queue/main.py
@@ -7,18 +7,19 @@ generator = TaskGenerator()
 processor = Processor()
 cash = MyStack()
 
-for i in range(25):
+for i in range(50):
     generator.gen_task()
 
 while generator.get_task() is not None:
+    task = generator.get_task()
     if processor.idle_proc():
         if not generator.none_task():
-            processor.add_task(generator.get_task())
+            processor.add_task(task)
         elif not cash.check_empty():
             processor.add_task(cash.pop())
     else:
-        if not generator.queue2.check_empty():
-            cash.push(generator.queue2.pop())
+        cash.push(task)
+    print('Stack before:', cash)
     print('Tasks\n', generator)
-    print('Stack', cash)
+    print('Stack after:', cash)
     processor.running()
