30d7539b73bb931fac7e316e653d7b6dc60364f2
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-06-02 10:08:38 +0300

    some fix

--- Stack_and_Queue/main.py
+++ Stack_and_Queue/main.py
@@ -8,7 +8,7 @@ processor = Processor()
 for i in range(50):
     generator.gen_task()
 
-while generator.get_task() is not None:
+while True:
     task = generator.get_task()
     if processor.idle_proc():
         if not generator.none_task():
@@ -16,5 +16,8 @@ while generator.get_task() is not None:
         elif not processor.wait.check_empty():
             processor.add_task(processor.wait.pop())
     print('Tasks\n', generator)
+    print('Processor:\n', processor)
     print('Stack:', processor.wait)
     processor.running()
+    if generator.none_task() and processor.wait.check_empty() and processor.thread1.idle and processor.thread2.idle:
+        break
