94fae8f2e3669c1d5a5b9f3f812f281e8e820e12
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-06-08 12:57:13 +0300

    some fix

--- Tests/func_plot.py
+++ Tests/func_plot.py
@@ -3,16 +3,14 @@ import matplotlib.pyplot as plt
 
 
 def func(x):
-    return np.sqrt((10 ** 10) / ((10 ** 4 - 4 * np.pi ** 2 * x ** 2) + 4 * np.pi ** 2 * x ** 2))
+    return np.sqrt((10 ** 10) / ((10 ** 4 - 4 * np.pi ** 2 * x ** 2) ** 2 + 4 * np.pi ** 2 * x ** 2))
 
 
-x = np.linspace(10**8, 10**12, 10**9, dtype=np.float64)
-y = np.float64(func(x))
+x = np.linspace(0, 10**1.72, 10**2)
+y = func(x)
 print(len(x))
 
 plt.loglog(x, y, 'g')
-#plt.ylabel('Время')
-#plt.xlabel('Длина массива')
 plt.title("Модуль передаточной функции")
 plt.tight_layout()
 plt.grid()
